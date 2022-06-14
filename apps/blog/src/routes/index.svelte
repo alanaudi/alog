0314bd9634476cc0674bf98bc7061598f846c4c6
alanaudi <alanaudi.tw@gmail.com>
2022-06-14 21:34:11 +0800

    feat: add unocss reset

--- apps/blog/src/routes/index.svelte
+++ apps/blog/src/routes/index.svelte
@@ -1,5 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="font-black text-4xl">
+<h1 class="font-bold text-4xl">Welcome to SvelteKit</h1>
+<p class="font-semibold text-2xl">
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
 
