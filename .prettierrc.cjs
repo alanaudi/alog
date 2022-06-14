29204f193abe2b5b55ac7162e9138d3de16892f9
alanaudi <alanaudi.tw@gmail.com>
2022-06-14 20:31:06 +0800

    chore: unify prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  proseWrap: 'always',
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  svelteSortOrder: 'options-scripts-markup-styles',
+}
