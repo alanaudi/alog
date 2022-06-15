653039f3fa1fc3e6caea04ff3f34762c3f4173cc
alanaudi <alanaudi.tw@gmail.com>
2022-06-15 15:20:11 +0800

    fix: main initial height vibration
    Closes #2

--- apps/blog/src/routes/index.svelte
+++ apps/blog/src/routes/index.svelte
@@ -1,24 +1,16 @@
 <script lang="ts">
   import { ALOG_VERSION } from '$utils'
   import { Footer, Nav } from '$components'
-  import { footerHeight, navHeight } from '$stores'
 </script>
 
 <Nav />
-<main
-  style={`--current-nav-height: ${$navHeight}px;` +
-    `--current-footer-height: ${$footerHeight}px;`}
-  class="grid place-items-center"
->
+<main class="grid place-items-center">
   <p class="">App Version: {ALOG_VERSION}</p>
 </main>
 <Footer />
 
 <style lang="scss">
   main {
-    @apply bg-black/10;
-    min-height: calc(
-      100vh - var(--current-nav-height) - var(--current-footer-height)
-    );
+    @apply flex-1 bg-black/10;
   }
 </style>
