a2aac7882ed015f6469e52221026daa84f5c42e0
alanaudi <alanaudi.tw@gmail.com>
2022-06-16 14:06:03 +0800

    feat: update home

--- apps/blog/src/routes/index.svelte
+++ apps/blog/src/routes/index.svelte
@@ -1,16 +1,44 @@
 <script lang="ts">
   import { ALOG_VERSION } from '$utils'
-  import { Footer, Nav } from '$components'
+  import { Footer, Nav, Tag } from '$components'
+  import { writable } from 'svelte/store'
+  const divClick = writable(0)
+
+  const tags = [
+    {
+      name: 'svelte',
+      icon: 'svgporn:svelte',
+    },
+    {
+      name: 'unocss',
+      icon: 'svgporn:unocss',
+    },
+  ]
+
+  const handleClick = (e: MouseEvent) => {
+    const tag = (e.target as HTMLElement).closest('a')
+    console.log((tag as HTMLElement).dataset.group)
+    divClick.set($divClick + 1)
+  }
 </script>
 
 <Nav />
-<main class="grid place-items-center">
+<main class="flex flex-col px-8">
   <p class="">App Version: {ALOG_VERSION}</p>
+  <h2>Tags</h2>
+  <h1>{$divClick}</h1>
+  <div on:click={handleClick}>
+    <div class="flex space-x-2">
+      {#each tags as { name, icon } (name)}
+        <Tag group="home" {name} {icon} />
+      {/each}
+    </div>
+  </div>
 </main>
 <Footer />
 
 <style lang="scss">
   main {
-    @apply flex-1 bg-black/10;
+    @apply flex-1;
   }
 </style>
