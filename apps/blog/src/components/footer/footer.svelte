f55039ae26972f5e77fe65408307bf4f213fc46f
alanaudi <alanaudi.tw@gmail.com>
2022-06-16 17:25:32 +0800

    feat: add theme switch

--- apps/blog/src/components/footer/footer.svelte
+++ apps/blog/src/components/footer/footer.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import './footer.scss'
   import PoweredByVercel from './powered-by-vercel.svelte'
+  import ThemeSwitch from './theme-switch.svelte'
   import { footerHeight } from '$stores'
 </script>
 
 <footer bind:offsetHeight={$footerHeight}>
   <PoweredByVercel />
-  <p>This is footer.</p>
+  <ThemeSwitch />
 </footer>
