305b940d72436aaa0f92555bf0ae66c45dd30ffa
alanaudi <alanaudi.tw@gmail.com>
2022-06-22 16:27:52 +0800

    feat: add title to error page

--- apps/blog/src/routes/__error.svelte
+++ apps/blog/src/routes/__error.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import '../styles/404.route.scss'
+  import { Helmet } from '$components'
 </script>
 
+<Helmet title="404" />
 <main class="error-page">
   <h1>404</h1>
   <h1>This page could not be found.</h1>
